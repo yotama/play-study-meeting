36cc5caffba2794966bf05d9597adf0b9caf6326
mythosil <mythosil@yahoo.co.jp>
2013-05-16 01:00:12 +0900

    initial commit

---
+++ todolist/app/controllers/Application.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+
+object Application extends Controller {
+  
+  def index = Action {
+    Redirect(routes.Application.tasks)
+  }
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+}
